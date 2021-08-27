7366885e2ce3be746bdc7270bdba3bee91fed7f9
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-08-27 12:53:21 +0300

    cors errors

--- backend/app.js
+++ backend/app.js
@@ -46,6 +46,7 @@ app.use(function (req, res, next) {
   const requestHeaders = req.headers["access-control-request-headers"];
   if (allowedCors.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
+    res.header("Access-Control-Allow-Credentials", true);
   }
   if (method === "OPTIONS") {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
