be4aaea0b68ca7a255573766fb62c866ced7ccdf
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-08-28 16:35:09 +0300

    token

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,7 @@
 class Api {
-  constructor({ address }) {
+  constructor({ address, token }) {
     this._address = address;
+    this._token = token;
   }
 
   _getAnswer(res) {
@@ -11,16 +12,20 @@ class Api {
   }
 
   getUserData() {
-    return fetch(`${this._address}/users/me `, { credentials: "include" }).then(
-      (res) => this._getAnswer(res)
-    );
+    return fetch(`${this._address}/users/me `, {
+      credentials: "include",
+      headers: {
+        Authorization: this._token,
+      },
+    }).then((res) => this._getAnswer(res));
   }
 
   editProfile(data) {
-    return fetch(`${this._address}/users/me `, {
-      method: "PATCH",
+    return fetch(`${this._address}$/users/me `, {
       credentials: "include",
+      method: "PATCH",
       headers: {
+        Authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ name: data.name, about: data.about }),
@@ -29,9 +34,10 @@ class Api {
 
   updateImage(data) {
     return fetch(`${this._address}/users/me/avatar `, {
-      method: "PATCH",
       credentials: "include",
+      method: "PATCH",
       headers: {
+        Authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ avatar: data.avatar }),
@@ -40,9 +46,10 @@ class Api {
 
   makeCard(data) {
     return fetch(`${this._address}/cards`, {
-      method: "POST",
       credentials: "include",
+      method: "POST",
       headers: {
+        Authorization: this._token,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ name: data.name, link: data.link }),
@@ -50,29 +57,41 @@ class Api {
   }
 
   getCards() {
-    return fetch(`${this._address}/cards`, { credentials: "include" }).then(
-      (res) => this._getAnswer(res)
-    );
+    return fetch(`${this._address}/cards`, {
+      credentials: "include",
+      headers: {
+        Authorization: this._token,
+      },
+    }).then((res) => this._getAnswer(res));
   }
 
   deleteCard(id) {
     return fetch(`${this._address}/cards/${id}`, {
-      method: "DELETE",
       credentials: "include",
+      method: "DELETE",
+      headers: {
+        Authorization: this._token,
+      },
     }).then((res) => this._getAnswer(res));
   }
 
   likeOn(id) {
     return fetch(`${this._address}/cards/likes/${id}`, {
-      method: "PUT",
       credentials: "include",
+      method: "PUT",
+      headers: {
+        Authorization: this._token,
+      },
     }).then((res) => this._getAnswer(res));
   }
 
   likeOff(id) {
     return fetch(`${this._address}/cards/likes/${id}`, {
-      method: "DELETE",
       credentials: "include",
+      method: "DELETE",
+      headers: {
+        Authorization: this._token,
+      },
     }).then((res) => this._getAnswer(res));
   }
 
@@ -86,7 +105,8 @@ class Api {
 }
 
 const config = {
-  address: "http://api.project.mesto.nomoredomains.rocks",
+  address: "http://project.mesto.nomoredomains.club",
+  //token: "22c6286b-d5fa-40bf-b483-a71816fa51e0",
 };
 
 const api = new Api(config);
