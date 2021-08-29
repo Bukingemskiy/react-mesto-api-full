0d43336a00aad3d77ae20e2e49b01559b149c922
Bukingemskiy <77937483+Bukingemskiy@users.noreply.github.com>
2021-08-29 13:54:47 +0300

    NewCard.data

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -81,17 +81,13 @@ function App() {
 
   function handleCardLike(card) {
     const isLiked = card.likes.some((i) => i === currentUser._id);
-    console.log(isLiked);
-    console.log(card._id);
-    console.log(card.likes);
-    console.log(currentUser._id);
     api
       .changeLikeCardStatus(card._id, !isLiked)
       .then((newCard) => {
-        console.log(newCard);
+        console.log(newCard.data);
         console.log(cards);
         setCards((cards) =>
-          cards.map((c) => (c._id === card._id ? newCard : c))
+          cards.map((c) => (c._id === card._id ? newCard.data : c))
         );
         console.log(cards);
         console.log(card._id);
